d9213b095b0cc709a79f98674eb76613ad664218
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-08-25 23:53:52 -0300

    feat: implement data seed

---
+++ src/models/prisma/data.ts
@@ -0,0 +1,52 @@
+const dataSeedDb = [
+  {
+    name: 'Espada curta',
+    amount: '13 Unidades',
+    url: 'https://i.pinimg.com/originals/6e/72/97/6e72971137c4c3d72d26552d220fa0a0.jpg',
+    price: '1 silver coin',
+  },
+  {
+    name: 'Faca dos micropósitos',
+    amount: '4 unidades',
+    url: 'https://varievo.com/wp-content/uploads/Faca-enferrujada.jpg',
+    price: '9 cooper coins',
+  },
+  {
+    name: 'Varinha da luz',
+    amount: '12 unidades',
+    url: 'https://images-americanas.b2w.io/produtos/01/00/img/1242114/9/1242114908_1GG.jpg',
+    price: '7 cooper coins',
+  },
+  {
+    name: 'Arco velho',
+    amount: '14 unidades',
+    url: 'http://gremoryrpg.weebly.com/uploads/2/5/1/1/25115387/418713835.jpg?223',
+    price: '8 cooper coins',
+  },
+  {
+    name: 'Excalibur',
+    amount: '1 unidade',
+    url: 'http://gremoryrpg.weebly.com/uploads/2/5/1/1/25115387/263716807.jpg?297',
+    price: '340 gold coins',
+  },
+  {
+    name: 'Adaga sagrada de Gwenhwyfar',
+    amount: '1 unidade',
+    url: 'http://gremoryrpg.weebly.com/uploads/2/5/1/1/25115387/654482372.jpg?345',
+    price: '320 gold coins',
+  },
+  {
+    name: 'Cajado Avaloniano de Merlin',
+    amount: '1 unidade',
+    url: 'http://gremoryrpg.weebly.com/uploads/2/5/1/1/25115387/708792596.png',
+    price: '330 gold coins',
+  },
+  {
+    name: 'Arco Retorcido de Morgana',
+    amount: '1 unidade',
+    url: 'https://i.pinimg.com/originals/b5/12/58/b51258d5f59626270894bed1e56afe30.png',
+    price: '350 gold coins',
+  },
+];
+
+export default dataSeedDb;

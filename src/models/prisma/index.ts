7ca56c5af68548947f298554c9c4d20fda9011d3
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-04-25 16:19:52 -0300

    feat: database URL

--- src/models/prisma/index.ts
+++ src/models/prisma/index.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD } = process.env;
+const { DATABASE_URL } = process.env;
 
 const prismaClient = new PrismaClient({
   datasources: {
     db: {
-      url: `mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:3306/Trybesmith`,
+      url: DATABASE_URL,
     },
   },
 });

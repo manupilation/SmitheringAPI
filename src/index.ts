d12a497b6b2112b68eefe1f8da50b001d7405423
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-04-25 15:35:31 -0300

    feat: index port listener

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
 import app from './app';
 
-const PORT = 3000;
+dotenv.config();
 
-const server = app.listen(PORT, () => console.log(
+const { PORT } = process.env || 3000;
+
+const server = app.listen(Number(PORT), '0.0.0.0', () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
 
